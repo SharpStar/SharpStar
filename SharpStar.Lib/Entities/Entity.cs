dc2496a6df16b229618ce4ede252f618a086f6fb
Mitchell Kutchuk <mitch528@gmail.com>
2014-04-10 08:45:52 -0700

    Added a ProjectileEntity class which holds information about projectiles

--- SharpStar.Lib/Entities/Entity.cs
+++ SharpStar.Lib/Entities/Entity.cs
@@ -1,4 +1,7 @@
-﻿using SharpStar.Lib.Misc;
+﻿using System;
+using System.IO;
+using SharpStar.Lib.DataTypes;
+using SharpStar.Lib.Misc;
 using SharpStar.Lib.Networking;
 
 namespace SharpStar.Lib.Entities
@@ -22,9 +25,36 @@ namespace SharpStar.Lib.Entities
 
             int discarded;
 
-            Entity ent = new Entity();
-            ent.EntityType = (EntityType)stream.ReadUInt8();
-            ent.StoreData = stream.ReadUInt8Array();
+            EntityType et = (EntityType)stream.ReadUInt8();
+
+            byte[] storeData = stream.ReadUInt8Array();
+
+            Entity ent;
+
+            if (et == EntityType.Projectile)
+            {
+
+                ProjectileEntity pent = new ProjectileEntity();
+
+                using (MemoryStream ms = new MemoryStream(storeData))
+                {
+                    using (StarboundStream s = new StarboundStream(ms))
+                    {
+                        pent.Projectile = s.ReadString();
+                        pent.Information = s.ReadVariant().Value as VariantDict;
+                    }
+                }
+
+                ent = pent;
+
+            }
+            else
+            {
+                ent = new Entity();
+            }
+
+            ent.EntityType = et;
+            ent.StoreData = storeData;
             ent.EntityId = stream.ReadSignedVLQ(out discarded);
 
             return ent;
---
+++ SharpStar.Lib/Entities/ProjectileEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpStar.Lib.DataTypes;
+
+namespace SharpStar.Lib.Entities
+{
+    public class ProjectileEntity : Entity
+    {
+
+        public string Projectile { get; set; }
+
+        public VariantDict Information { get; set; }
+
+    }
+}

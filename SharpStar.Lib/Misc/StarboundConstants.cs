ca5fe747ebe2028c3f804836f2c36f78c3f0922d
Mitchell Kutchuk <mitch528@gmail.com>
2014-08-24 17:01:56 -0700

    StarboundConstants somehow got missed in last commit

---
+++ SharpStar.Lib/Misc/StarboundConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpStar.Lib.Misc
+{
+    public static class StarboundConstants
+    {
+
+        public const int ProtocolVersion = 643;
+
+    }
+}
